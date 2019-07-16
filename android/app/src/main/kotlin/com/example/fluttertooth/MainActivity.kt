3c0418840d6c556d5ad9117988c5d175ad6b38ef
Oleksii Kalentiev <oleksii@getaround.com>
2019-07-16 15:15:04 +0200

    Step 5: Add method channel to the Android app and fetch bluetooth status.

--- android/app/src/main/kotlin/com/example/fluttertooth/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fluttertooth/MainActivity.kt
@@ -1,13 +1,45 @@
 package com.example.fluttertooth
 
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
-
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterView, "flutter.getaround.com/bluetooth").setMethodCallHandler { call, result ->
+            if (call.method == "getBluetoothState") {
+                result.success(getBluetoothState())
+            } else {
+                result.notImplemented()
+            }
+        }
+
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+
+    private fun getBluetoothState(): String? {
+        val bluetoothState: String?
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        when {
+            mBluetoothAdapter == null -> {
+                bluetoothState = "Not Supported"
+            }
+            mBluetoothAdapter.state == BluetoothAdapter.STATE_ON -> {
+                bluetoothState = "Turned On"
+            }
+
+            mBluetoothAdapter.state == BluetoothAdapter.STATE_OFF -> {
+                bluetoothState = "Turned Off"
+            }
+
+            else -> {
+                bluetoothState = "Unknown"
+            }
+        }
+        return bluetoothState
+    }
 }
