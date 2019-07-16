32f9bbb793b6ffc2ab81900132aa7328c8347aac
Oleksii Kalentiev <oleksii@getaround.com>
2019-07-16 15:14:29 +0200

    Step 4: Add method channel to the iOS app and fetch bluetooth status.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,48 @@
 import UIKit
 import Flutter
+import CoreBluetooth
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    var bluetoothPeripheralManager: CBCentralManager?
+    var bluetoothState: String?
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let options = [CBCentralManagerOptionShowPowerAlertKey: false]
+        bluetoothPeripheralManager = CBCentralManager(delegate: self, queue: nil, options: options)
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let bluetoothChannel = FlutterMethodChannel(name: "flutter.getaround.com/bluetooth",
+                                                    binaryMessenger: controller)
+        bluetoothChannel.setMethodCallHandler { (call, result) in
+            if (call.method == "getBluetoothState") {
+                result(self.bluetoothState)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
+
+extension AppDelegate: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOn:
+            bluetoothState = "Turned On"
+        case .poweredOff:
+            bluetoothState = "Turned Off"
+        case .resetting:
+            bluetoothState = "Resetting"
+        case .unauthorized:
+            bluetoothState = "Not Authorized"
+        case .unsupported:
+            bluetoothState = "Not Supported"
+        case .unknown:
+            bluetoothState = "Unknown"
+        default:
+            break
+        }
+    }
 }
